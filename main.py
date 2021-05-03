4fbdad7cbaa4bbc231afaabca263901f6701fd15
Lauro Brant <brantlauro@gmail.com>
2021-05-03 16:57:02 -0300

    update main.py

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ from random import randint
 choice = ' '
 while True:
     while choice not in 'NY':
-        choice = input('Do you want to roll a 6 face dice? [Y/N] ').upper().strip()[0]
+        choice = input('Do you want to roll a dice? [Y/N] ').upper().strip()[0]
         if choice == 'N':
             break
             choice = ' '
         if choice == 'Y':
-            dice = randint(1, 6)
+            choice_dice = 0
+            while choice_dice not in (4, 6, 8, 10, 12, 20, 100): 
+                choice_dice = int(input('What dice do you want to roll? [4/6/8/10/12/20/100] '))
+            dice = randint(1, choice_dice)
             print(dice)
+            dice = 0
+            choice_dice = ' '
             choice = ' '
